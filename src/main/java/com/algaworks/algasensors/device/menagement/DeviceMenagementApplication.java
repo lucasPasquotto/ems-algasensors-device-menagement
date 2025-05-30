3adad9c7a76529b6f9188c84e38fe89de76f792d
lucasPasquotto <lucaspasquotto@outlook.com>
2025-05-29 22:21:34 -0300

    New project files

---
+++ src/main/java/com/algaworks/algasensors/device/menagement/DeviceMenagementApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.device.menagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMenagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMenagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algasensors/device/menagement/DeviceMenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.device.menagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMenagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
